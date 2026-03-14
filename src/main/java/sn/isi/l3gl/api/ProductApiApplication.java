318b9f4bdbcd63da1f34f99789452fb41f8a74d1
zarequiem <ababacardiagne25@gmail.com>
2026-03-14 17:41:23 +0000

    feat: product-api - contrôleur REST complet version 0.0.1-SNAPSHOT

--- src/main/java/sn/isi/l3gl/api/ProductApiApplication.java
+++ src/main/java/sn/isi/l3gl/api/ProductApiApplication.java
@@ -2,12 +2,17 @@ package sn.isi.l3gl.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "sn.isi.l3gl.api",
+        "sn.isi.l3gl.core"
+})
+@EntityScan(basePackages = "sn.isi.l3gl.core.entity")
+@EnableJpaRepositories(basePackages = "sn.isi.l3gl.core.repository")
 public class ProductApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProductApiApplication.class, args);
     }
-
 }
