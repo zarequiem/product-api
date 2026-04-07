f40423305e576f50c89b1da808e663d9015fe72b
zarequiem <ababacardiagne25@gmail.com>
2026-04-07 01:07:50 +0000

    fix: ajout H2 pour tests CI et correction test unitaire

--- src/test/java/sn/isi/l3gl/api/ProductApiApplicationTests.java
+++ src/test/java/sn/isi/l3gl/api/ProductApiApplicationTests.java
@@ -3,7 +3,12 @@ package sn.isi.l3gl.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb",
+                "spring.datasource.driver-class-name=org.h2.Driver",
+                "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+        })
 class ProductApiApplicationTests {
 
     @Test
